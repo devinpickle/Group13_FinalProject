206353965aed58c30fdbf46e23cc8245bd80ce92
Devin Picklesimer <devinpickle>
2021-06-25 17:48:21 -0600

    userControl

--- rename/game/director.py
+++ rename/game/director.py
@@ -2,6 +2,8 @@ import arcade
 from game import constants
 from game.hero import Player
 from game.ground import Ground
+from game.arcade_output_service import ArcadeOutputService
+#from game.move_actors import MoveActors
 
 class Director(arcade.Window):
 
@@ -10,10 +12,13 @@ class Director(arcade.Window):
 
         super().__init__(constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT, constants.SCREEN_TITLE)
 
+        self.output_service = ArcadeOutputService()
+
         # Lists of sprites
         self.player_list = None
         self.wall_list = None
         
+        self.physics_engine = None
 
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
 
@@ -27,14 +32,48 @@ class Director(arcade.Window):
         self.player_sprite = Player()
         self.player_list.append(self.player_sprite)
 
+        
         # Create the ground
         for x in range(0, 1250, 64):
             wall = Ground(x, 32)
             self.wall_list.append(wall)
 
+        
+        # Create the physics engine
+        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite, self.wall_list, constants.GRAVITY)
+
+
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+        
+        if key == arcade.key.UP or key == arcade.key.W or key == arcade.key.SPACE:
+            if self.physics_engine.can_jump():
+                self.player_sprite.change_y = constants.PLAYER_JUMP_SPEED
+        #elif key == arcade.key.DOWN or key == arcade.key.S:
+            #self.player_sprite.change_y = -constants.PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = -constants.PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = constants.PLAYER_MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        if key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+
     def on_draw(self):
-        arcade.start_render()
+        
+        self.output_service.clear_screen()
+        self.output_service.draw_actors(self.wall_list)
+        self.output_service.draw_actors(self.player_list)
 
-        self.wall_list.draw()
-        self.player_list.draw()
 
+    def on_update(self, delta_time):
+        """ Movement and game logic """
+
+        # Move the player with the physics engine
+        self.physics_engine.update()
