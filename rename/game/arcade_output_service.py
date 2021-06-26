ee35d9ab1ea4f6585d25f741b6c99293eb4fc963
Brayden-Jones74 <jon20015@byui.edu>
2021-06-25 19:47:58 -0600

    Updated docstrings

--- rename/game/arcade_output_service.py
+++ rename/game/arcade_output_service.py
@@ -2,6 +2,14 @@ import sys
 import arcade
 
 class ArcadeOutputService:
+    """Renders and draws screen with actors. Capable of clearing the screen, and updating it with new actor positions.
+
+    Stereotype:
+        Service Provider
+    
+    Attributes:
+        none
+    """
 
 
     def __init__(self):
@@ -10,11 +18,29 @@ class ArcadeOutputService:
         pass
 
     def clear_screen(self):
+        """Starts rendering of screen.
+        
+        Args:
+            self (ArcadeOutputService): an instance of ArcadeOutputService
+        """
+
         arcade.start_render()
 
     def draw_actor(self, actor):
+        """Draws on actor on the screen. 
+
+        Args: 
+            self (ArcadeOutputService): an instance of ArcadeOutputService
+            actor (Sprite): an instance of a sprite
+        """
         actor.draw()
 
     def draw_actors(self, actors):
+        """Draws multiple actors on the screen. 
+
+        Args:
+            self (ArcadeOutputService): an instance of ArcadeOutputService
+            actors (Sprites): instances of multiple sprites
+        """
         for actor in actors:
             self.draw_actor(actor)
