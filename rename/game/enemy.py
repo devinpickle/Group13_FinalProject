959af605892688672719bd8384e4c1135b8de9c4
Brayden-Jones74 <jon20015@byui.edu>
2021-07-03 19:52:30 -0600

    Beginning structure, all 4 enemies

--- rename/game/enemy.py
+++ rename/game/enemy.py
@@ -37,7 +37,7 @@ class Enemy(arcade.Sprite, ABC):
     #What an enemy does before attacking; general movement
     @abstractmethod
     def enemy_movement_auto(self):
-        """Controls enemy movement before any attack is triggered. Can be overrided to have none, and only target player the entire time.
+        """Controls enemy movement before any attack is triggered; autonomous movement. Can be overrided to have none, and only target player the entire time.
         All Enemy classes must have this method present.
         
         Args:
@@ -63,7 +63,6 @@ class FloatingEnemy(Enemy):
         pass
 
     
-
 class DeathWorm(Enemy):
     """A code template for a Death Worm enemy. Simply moves up and down, and does not react to player."""
 
@@ -84,12 +83,50 @@ class DeathWorm(Enemy):
         #print("\nI have no triggering event.\n")
         pass
     
-    
+
+class InsectEnemy(Enemy):
+    """A code template for an insect enemy that clings to platforms. Pre-programmed to walk in a certain direction and not react to player.""" 
+
+    def __init__(self):
+        super().__init__()
+        self._enemy_health = 10 #not as much health as other enemies
+
+    #overriding second abstract method
+    def enemy_movement_auto(self):
+        #print("I walk around, cling to platforms, and to edges/walls.")
+        pass
+
+    #overriding abstract method
+    def enemy_attack_triggering_event(self):
+        #print("there is no triggering event for this enemy; they simply move in a repeatable pattern")
+        pass
+
+
+class LaserShooterEnemy(Enemy):
+    """A code template for an enemy that hangs from the bottom of a platform and shoots at the player.
+    Keeps track of player position and targets them with shots."""
+
+    def __init__(self):
+        super().__init__()
+        self._enemy_move_speed = 0 #this enemy does not move anywhere
+        self._angle = 45
+        
+    #overriding second abstract method
+    def enemy_movement_auto(self):
+        #print("I do not move anywhere; stays in one spot.")
+        pass
+
+    #overriding abstract method
+    def enemy_attack_triggering_event(self):
+        #print("If the player gets close enough, which still can be a little far off, I will shoot at them.")
+        pass
+        
 
 
 
 
-# #code to test classes are working properly
+
+# #code to test classes are working properly (with print statements active)
 # ghastly_beam_shooter = FloatingEnemy()
 # ghastly_beam_shooter.enemy_movement_auto()
 # ghastly_beam_shooter.enemy_attack_triggering_event()
@@ -99,3 +136,10 @@ class DeathWorm(Enemy):
 # death_worm = DeathWorm()
 # death_worm.enemy_movement_auto()
 # death_worm.enemy_attack_triggering_event()
+
+# enemy_insect = InsectEnemy()
+# enemy_insect.enemy_movement_auto()
+# enemy_insect.enemy_attack_triggering_event()
+
+# deadly_shooter = LaserShooterEnemy()
+# print(deadly_shooter.angle)
